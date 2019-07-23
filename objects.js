b1507e98b46a39701237e9dda23be4576f9914d9
mlessin <52704887+mlessin@users.noreply.github.com>
2019-07-23 18:17:38 +0300

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,38 @@
+document.write("I am connected");
+var name="john doe";
+document.write("<br/>");
+document.write(name);
+
+var person ={
+    name: "john doe",
+    age: 120,
+    gender: "male"
+};
+document.write("my name is"+ person.name);
+document.write("<br/>");
+document.write("my name is"+person.age);
+document.write("<br/>");
+document .write("my name is"+person.gender);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
