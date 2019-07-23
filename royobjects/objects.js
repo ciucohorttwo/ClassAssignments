2635072ee4d5d7f789ee4ed908f6b33a7c6e2145
mlessin <52704887+mlessin@users.noreply.github.com>
2019-07-23 20:35:38 +0300

    Add files via upload

---
+++ royobjects/objects.js
@@ -0,0 +1,94 @@
+// my five examples of objects.
+//my first object.
+var fruit ={
+    name: "apple",
+    color: "green",
+    seed: "black",
+    taste: function()
+    {
+var sweet ="hmmmmmmmm!"
+return sweet
+    }
+}
+document.write(fruit.name);
+document.write("<br/>");
+document.write(fruit.color);
+document.write("<br/>");
+document.write(fruit.seed);
+document.write("<br/>");
+document.write(fruit.taste());
+document.write("<br/>");
+//my second object.
+var cow ={
+    name:"gashu",
+    breed: "fresian",
+    color:"brown",
+    sound: function()
+    {
+        var cry ="mooooooooooo!"
+        return cry
+    }
+}
+document.write(cow.name);
+document.write("<br/>");
+document.write(cow.breed);
+document.write("<br/>");
+document.write(cow.color);
+document.write("<br/>");
+document.write(cow.sound());
+document.write("<br/>");
+
+// my third object.
+var house={
+    door:"wooden",
+    window:"glass",
+    ventiletors:12,
+    welcome: function()
+    {
+        var visit ="you are most welcome dear"
+        return visit
+    }
+}
+document.write(house.door);
+document.write("<br/>");
+document.write(house.window);
+document.write("<br/>");
+document.write(house.ventiletors);
+document.write("<br/>");
+document.write(house.welcome());
+document.write("<br/>");
+
+//my fourth object.
+var phone={
+name:"tecno",
+type:"camon",
+
+puporse: function()
+{
+    var press ="power button"
+    return press 
+}
+}
+document.write(phone.name);
+document.write("<br/>");
+document.write(phone.type);
+document.write("<br/>");
+document.write(phone.puporse());
+document.write("<br/>");
+
+//my fifth object.
+var book={
+    type:"picfure",
+    page:200,
+    open: function()
+    {
+        var use ="turn right"
+        return use
+    }
+}
+document.write(book.type);
+document.write("<br/>");
+document.write(book.page);
+document.write("<br/>");
+document.write(book.open());
+
