829f75cad62f94efc30b036449279c849e699fbb
Strewik <52960396+Strewik@users.noreply.github.com>
2019-08-05 16:25:42 +0300

    Add files via upload

---
+++ Facebook login assignment/jsTraining.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+document.write("i am connected!");
+
+
+var name;
+name = "john doe!!";
+document.write("</br > john doe");
+
+document.write(name);                                                               
